dd1deff958ecf3f43756215970dffd4f8fc68b38
Reis <joaosimoesreis.jsr@gmail.com>
2023-01-30 15:33:21 +0000

    useless comments removed

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -10,27 +10,10 @@ module.exports = new Event("ready", client => {
 	console.log("(Client Status) - Ready");
 	console.log("----------------------------");
 
-
-	/*
-	client.user.setPresence({
-		status: 'invisible',
-	   });
-	*/
-
 	const online = new Discord.MessageEmbed()
 	.setDescription('🟢 **ONLINE**')
 	.setColor(color.clientOnline)
 
 	client.channels.cache.get(config.channel_status).send({ embeds : [online]})
-
-/*
-	let reminder = new cron.CronJob('39 * * * *', () => {
-			client.users.cache.get("331469231730458624").send('**Waifus Reset**');
-		  });
-			  
-
-		  
-	reminder.start()
-*/
 
 });
